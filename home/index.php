dfc1b7911f0c9122ae6d32c9de6d71d532652613
kinokou <87245605+kinokou@users.noreply.github.com>
2021-07-13 06:24:37 +0900

    initial commit.
    練習です。

---
+++ home/index.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+ <head>
+  <meta charset="UTF-8">
+  <title>nissyblog</title>
+  <meta name="description" content="大学院生のリアルをお届け">
+<!--  <meta name="viewport" content="width=device-width,initial-scale=1"</meta>-->
+  <link rel="stylesheet" href="style.css">
+ </head>
+ <body>
+<!-- ヘッダーとメニューの設定 -->
+  <header>
+    <div class="header-wrapper">
+     <h1><a class="header-title" href="style.html">にしきの blog</a></h1>
+     <div class="main-navi">
+       <ul>
+         <li><a href="index.html">ホーム</a></li>
+         <li><a href="../self-introduction/self-introduction.html">大学院生特集</a></a></li>
+         <li><a href="">就活特集</a></li>
+         <li><a href="">環境問題特集</a></li>
+       </ul>
+      </div>
+    </div>
+  </header>
+
+  <!-- メインの設定 -->
+  <div class="main">
+    <div class="main-article">
+      <div class="article1">
+          <img class="article-img" src="sampleimg1.png" />
+          <div class="outline">
+            <h2>タイトル</h>
+            <p>ここに概要を描きます。</p>
+            <span class="category">カテゴリ１</span>
+            <span class="category">カテゴリ２</span>
+            <span class="date">日付</span>
+          </div>
+      </div>
+
+      <div class="article1">
+          <img class="article-img" src="sampleimg1.png" />
+          <div class="outline">
+            <h2>タイトル</h>
+            <p>ここに概要を描きます。</p>
+            <span class="category">カテゴリ１</span>
+            <span class="category">カテゴリ２</span>
+            <span class="date">日付</span>
+          </div>
+      </div>
+    </div>
+
+    <div class="sub-article">
+
+      <div class="self-introduction">
+        <h3>自己紹介</h3>
+        <div class="nishi-introduction">
+          <p>にっしー</p>
+          <p>ここで自己紹介をします。</p>
+        </div>
+         <div class="kino-introduction">
+         <p>きのこう</p>
+         <p>ここで自己紹介をします</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+ </body>
+</html>
